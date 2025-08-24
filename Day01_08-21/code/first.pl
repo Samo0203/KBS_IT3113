0ef0d5953420fd40ca3e7e345496cb2a5a6394e8
Samodya <samodya0203samadini@gmail.com>
2025-08-24 19:23:49 +0530

    Add files via upload

---
+++ Day01_08-21/code/first.pl
@@ -0,0 +1,42 @@
+%Single objects
+food(pizza).
+bird(parrot).
+island(srilanka).
+animal(dog).
+fruit(kiwi).
+weapon(gun).
+hardware(keyboard).
+android_phone(samsung).
+male(kamal).
+student(jhon).
+black(elephant).
+shape(circle).
+vehicle(bus).
+
+%Multiple objects
+likes(john,ann).
+studies(charlie,ai).
+teachs(collins,cs).
+loves(vicent,mia).
+is_next_month(october,september).
+is_student_of(jane,roy).
+is_color(elephant,black).
+
+%Feed the knowledge about months
+has_month(name).
+has_month(order).
+has_month(numofdates).
+first_month(january).
+next_month(february,march).
+prev_month(may,april).
+sixth_month(june).
+after(july,june).
+month_8(august).
+
+month(january).
+month(february).
+month(march).
+
+num_of_days(january,31).
+num_of_days(february,28).
+
---
+++ Day01_08-21/code/second.pl
@@ -0,0 +1,7 @@
+is_parent_of(tom,bob).
+is_parent_of(pam,bob).
+is_parent_of(tom,liz).
+is_parent_of(bob,ann).
+is_parent_of(bob,pat).
+is_parent_of(pat,jim).
+
